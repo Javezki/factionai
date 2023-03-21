4dca224b08f140c3ee4bbbf2dc0ed8e12f669042
BuildTools <unconfigured@null.spigotmc.org>
2023-03-20 22:35:51 -0400

    temp files

--- factionai/src/test/java/com/github/javezki/AppTest.java
+++ factionai/src/test/java/com/github/javezki/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.github.javezki.sqlconfig.SQLConfig;
+
 /**
  * Unit test for simple App.
  */
@@ -12,9 +14,10 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        FactionMain app = new FactionMain();
+    public void shouldAnswerWithTrue() {
+        SQLConfig.connectToDB();
+        assertTrue("does DB exist?", SQLConfig.databaseExists("factiondb"));
     }
 }
